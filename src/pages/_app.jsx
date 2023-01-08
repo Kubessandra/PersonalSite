1845b2392f3e91739db23538d24945fe040bd1a8
Kubessandra <aymeric.beudaert@gmail.com>
2023-01-08 21:08:37 +0100

    fix analytics
    .

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
